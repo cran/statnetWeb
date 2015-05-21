47077d5454864d19e3027fc59455647cc7aefca6
Emily Beylerian <ebey@uw.edu>
2015-05-21 00:00:00 +0000

    version 0.3.4

---
+++ README.md
@@ -0,0 +1,61 @@
+statnetWeb
+==========
+
+This is an R-shiny interface for the statnet suite of R packages. It currently incorporates functionality from the ergm, network and sna packages, allowing users to fit and assess exponential random graph models via GUI.
+
+Run the app in two ways:  
+* Through the shinyapps server with this link: https://statnet.shinyapps.io/statnetWeb  
+* On your own machine using the statnetWeb R package:  
+    `install.packages("devtools")`  
+    `devtools::install_github("statnet/statnetWeb")`  
+    `library(statnetWeb)`  
+    `run_sw()`
+
+
+statnet wiki:  
+https://statnet.csde.washington.edu/trac 
+
+statnet packages on CRAN:  
+http://cran.r-project.org/web/packages/ergm/  
+http://cran.r-project.org/web/packages/network/  
+http://cran.r-project.org/web/packages/sna/
+
+#### New in v0.3.4
+
+* Conditional uniform graph test histograms in Network Descriptives
+* Descriptions of built-in networks
+* Minor bug fixes
+
+#### New in v0.3.3
+
+* Fix bug from v0.3.2 preventing built-in networks from loading
+* Prevent calls to search.ergmTerms() from printing to the console
+
+#### New in v0.3.2
+
+* App is compatible with Shiny v0.11.1.9002 (we have a new look!)
+* Only .rds files supported when saving and uploading from the R environment.
+* Issue solved where GOF plots from directed networks were not appearing.
+* Issue with uploading edge values solved.
+
+#### New in v0.3.1
+
+* Improved method of saving user-entered attributes
+* Bug in uploading relational data saved as R object is fixed
+* "Fit Model" button is closer to summary statistics
+* User can add terms to formula by pressing Enter key
+* User is not allowed to color code degree distribution of total degree for directed networks (in- and out-degree distributions are allowed)
+* Title of application is incorporated into title of first tab
+* Prevented error in viewing GOF comparison when no models are saved
+* New color palettes for attributes with more than nine levels
+
+#### New in v0.3.0
+
+* Access term documentation within Fit Model page
+* Customize MCMC control parameters when fitting a model or simulating from a model
+* Save up to five different models for a single network
+* Compare coefficients and AIC/BIC scores across models
+* Switch between models when looking at MCMC Diagnostics, GOF and Simulations
+* Compare GOF plots across saved models in a comprehensive chart
+* Plot simulation statistics compared to target statistics
+* Arrow icons for page navigation
---
+++ inst/shiny/global.R
@@ -0,0 +1,144 @@
+
+library(statnetWeb)
+
+# version of textInput with more size options.
+# specify class = 'input-small' or class='input-mini' in
+# addition to other textInput args
+customTextInput <- function(inputId, label, value = "",
+                            labelstyle = "dispay:inline;", ...) {
+  tagList(tags$label(label, `for` = inputId, style = labelstyle),
+          tags$input(id = inputId, type = "text", value = value,
+                     ...))
+}
+
+customNumericInput <- function(inputId, label, value = 0,
+                               labelstyle = "display:inline;", ...) {
+  tagList(tags$label(label, `for` = inputId, style = labelstyle),
+          tags$input(id = inputId, type = "number", value = value,
+                     ...))
+}
+
+# version of selectInput...shorter box and label
+# inline lapply allows us to add each element of
+# choices as an option in the select menu
+inlineSelectInput <- function(inputId, label, choices, ...) {
+  if(is.null(label)){
+    labeldisp <- "display: none;"
+  } else {
+    labeldisp <- "display: inline;"
+  }
+  
+  tagList(tags$label(label, `for` = inputId, style = labeldisp),
+          tags$select(id = inputId, choices = choices, ..., 
+                      class = "shiny-bound-input inlineselect", 
+                      lapply(choices, tags$option)))
+}
+
+# create a list of unique term names
+sink("NUL")  # prevents terms from printing to console
+allterms <- search.ergmTerms()
+sink()
+inds <- regexpr(pattern = "\\(", allterms)
+for (i in 1:length(allterms)) {
+  allterms[i] <- substr(allterms[[i]], start = 1,
+                        stop = inds[i] - 1)
+}
+allterms <- unique(allterms)
+
+# disable widgets when they should not be usable
+disableWidget <- function(id, session, disabled = TRUE) {
+  if (disabled) {
+    session$sendCustomMessage(type = "jsCode",
+                              list(code = paste("$('#", id, "').prop('disabled',true)",
+                                                sep = "")))
+  } else {
+    session$sendCustomMessage(type = "jsCode",
+                              list(code = paste("$('#", id, "').prop('disabled',false)",
+                                                sep = "")))
+  }
+}
+
+# function to return tests on simulated graphs
+cugstats <- function(x, term, directed, loops) {
+  nw <- network(x, directed = directed, loops = loops)
+  summary.statistics(as.formula(paste("nw ~ ", term)))
+}
+
+
+# Takes an ergm object and gathers some of the information from 
+# summary.ergm, in preparation to be passed to the function 
+# coef.comparison.
+ergm.info <- function(object) {
+  coefs <- object$coef
+  terms <- names(object$coef)
+  coefmatrix <- summary(object)$coefs
+  pval <- coefmatrix[, 4]
+  signif.stars <- symnum(pval, corr = FALSE, na = FALSE,
+                         cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                         symbols = c("***", "**", "*", ".", " "), legend = F)
+  starredcoef <- paste0(format(coefs, digits = 3), signif.stars)
+  
+  ans <- list()
+  count <- 1
+  for (i in terms) {
+    ans[i] <- starredcoef[count]
+    count <- count + 1
+  }
+  ans$AIC <- format(summary(object)$aic, digits = 3)
+  ans$BIC <- format(summary(object)$bic, digits = 3)
+  ans
+}
+
+
+# Takes in a list of multiple outputs from ergm.info, 
+# formatting it all into a table comparing up to 5 models.
+coef.comparison <- function(coeflist) {
+  nmod <- length(coeflist)
+  if (nmod == 0)
+    stop("No model summaries were passed to model.comparison")
+  if (nmod > 5)
+    stop("List of length greater than 5 passed to model.comparison")
+  allterms <- c()
+  for (j in 1:nmod) {
+    allterms <- append(allterms, names(coeflist[[j]]))
+  }
+  allterms <- unique(allterms)
+  allterms <- c(allterms[-which(allterms == "AIC")], "AIC")
+  allterms <- c(allterms[-which(allterms == "BIC")], "BIC")
+  mat <- matrix(nrow = length(allterms), ncol = nmod)
+  
+  for (k in 1:nmod) {
+    row <- 1
+    for (l in allterms) {
+      if (is.null(coeflist[[k]][[l]])) {
+        mat[row, k] <- "NA"
+      } else {
+        mat[row, k] <- coeflist[[k]][[l]]
+      }
+      row <- row + 1
+    }
+  }
+  
+  rownames(mat) <- allterms
+  colnames(mat) <- paste0("Model", 1:nmod)
+  return(print(mat, quote = FALSE))
+}
+
+stat.comparison <- function(statlist) {
+  nmod <- length(statlist)
+  if (nmod == 0)
+    stop("No model summaries were passed to stat.comparison")
+  if (nmod > 5)
+    stop("List of length greater than 5 passed to stat.comparison")
+  
+  statvec <- c()
+  for (j in 1:nmod) {
+    for (k in 1:length(statlist[[j]])) {
+      if (!(names(statlist[[j]][k]) %in% names(statvec))) {
+        statvec <- append(statvec, statlist[[j]][k])
+      }
+    }
+  }
+  
+  return(statvec)
+}
